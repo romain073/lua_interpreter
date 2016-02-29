f680bbcbd03bfd67ab2b54ea02a824ff57973b83
Romain FONTAINE <romain.fontaine73@gmail.com>
2016-02-29 11:02:35 +0000

    New test cases

--- partialtest.lua
+++ partialtest.lua
@@ -1,1 +1,94 @@
-a=13+(22-11)/1*44
+print "a"
+print "B"
+print "c"
+
+
+
+
+
+
+
+
+
+
+mytablefn{_abc=1, def=2, 1,{1,2,3,4}}
+
+
+
+
+
+
+
+
+
+a = b[c.index]
+
+a = function(a)
+    return a+1
+end
+
+mylist = {1,3,4,5,romain=10,[myfunc()]=4}
+
+
+
+
+
+
+local function myfunc()
+    local v1,v2,_3 = 1,4,true    
+    local _other
+end
+
+
+
+
+
+
+
+function myfunc()
+end
+function test.t.package:myfunc(...)
+   print "a" 
+end
+
+function myfunc(a, ...)
+end
+
+for a,b,c in 1,2,3 do
+    print 'a'
+end
+
+for a = 1,4,5 do
+    print "a"
+end
+for a = 1,4 do
+    print "a"
+end
+
+
+if true then
+    print "a"
+elseif false then
+    print "b"
+elseif nil then
+    print "c"
+else
+    print "d"
+end
+
+
+
+repeat 
+    print "a"
+until true
+
+
+while true do
+    print "a"
+end
+
+do
+    a=13+(22-11)/1*44
+    b=myfunction(1,c,mysecondfunction(a));
+    print "a"
+end
